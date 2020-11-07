f24201867d30ce0b70e38b67f3b99d040968eba8
Trọng Nghĩa Ngô <nghiango262@gmail.com>
2020-11-07 21:14:43 +0700

    fix loi show array empty 2 value '0' lien tiep

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 
-	"kaka.com/constant"
+	"kaka.com/maxConsecutiveOnes"
 )
 
 func main() {
-	v : = constant.Constant("Kakak")
-	fmt.Println(v)
+	input := []int {0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0}
+	maxx := maxConsecutiveOnes.FindMaxConsecutiveOnes(input)
+	fmt.Println(maxx)
 }
